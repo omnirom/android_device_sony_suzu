5eb5a6e81fd5704a4eacd0af330c92fb2b4fa953
Humberto Borba <humberos@gmail.com>
2016-07-28 21:01:03 -0300

    sony: suzu: Omnify
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I253d22d9bfea55468b0b0234ae3f82db098a4a40

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_f5121.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_suzu.mk
 
